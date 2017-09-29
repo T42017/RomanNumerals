124d1c064f64457dcbde84d309eac90ce88ccd6c
JohanWikstrom <johan.wikstrom@lund.ntig.se>
2017-09-29 09:54:59 +0200

    Added Console.writeline

--- RomanNumeralsConverter/RomanNumeralsConverter/Program.cs
+++ RomanNumeralsConverter/RomanNumeralsConverter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,22 @@ namespace RomanNumeralsConverter
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
+        String input,output;
+            int number;  
+            Console.WriteLine("Write your number in Arabic Numerals");
+
+            input = Console.ReadLine();
+            number = int.Parse(input);
+
+            
+
+
+            Console.WriteLine(insert +" is "+output+" in Roman Numberals");
+
+
         }
     }
 }
