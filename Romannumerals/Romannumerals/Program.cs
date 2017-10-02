93eec2c8098ab36dd3339989402037f38c675f9d
Marcus Linné <marcus.linne@lund.ntig.se>
2017-10-02 09:40:14 +0200

    finally working

--- Romannumerals/Romannumerals/Program.cs
+++ Romannumerals/Romannumerals/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,89 +10,114 @@ namespace Romannumerals
 {
     public static class Rom
     {
-        public static readonly Dictionary<char, int> LexikonRomerskaSiffror;
-        public static readonly Dictionary<int, string> LexikonNummerTillRomerska;
-
         public static void Main()
         {
-            Console.WriteLine("Skriv in det talet du vill konvertera tll romerska siffror");
-        }
-
-        static Rom()
-        {
-            LexikonRomerskaSiffror = new Dictionary<char, int>
+            while (true)
             {
-                {'M', 1000},
-                {'D', 500},
-                {'C', 100},
-                {'L', 50},
-                {'X', 10},
-                {'V', 5},
-                {'I', 1}
-            };
+                Console.Write("Skriv in det talet du vill konvertera tll romerska siffror: ");
+                Console.WriteLine();
+                int tal = int.Parse(Console.ReadLine());
 
-            LexikonNummerTillRomerska = new Dictionary<int, string>
-            {
-                {1, "I"},
-                {5, "V"},
-                {10, "X"},
-                {50, "L"},
-                {100, "C"},
-                {500, "D"},
-                {1000, "M"}
-            };
-        }
-
-        public static string Till(int tal)
-        {
-            var romersk = new StringBuilder();
-
-            foreach (var sak in LexikonNummerTillRomerska)
-            {
-                while (tal >= sak.Key)
-                {
-                    romersk.Append(sak.Value);
-                    tal -= sak.Key;
-                }
+                Console.WriteLine(ConvertNumToRoman(tal));
             }
-            return romersk.ToString();
+
+            Console.ReadKey();
         }
 
-        public static int TagenAv(string romersk)
+        public static string ConvertNumToRoman(int tal)
         {
-            int totalt = 0;
-            int nutid, tidigare =0;
-            char nutidRomersk, tidigareRomersk = '\0';
+            if (tal >= 1000) return "M" + ConvertNumToRoman(tal - 1000);
 
-            for (int i = 0; i < romersk.Length; i++)
-            {
-                nutidRomersk = romersk[i];
-                tidigare = tidigareRomersk != '\0' ? LexikonRomerskaSiffror[tidigareRomersk] : '\0';
-                nutid = LexikonRomerskaSiffror[nutidRomersk];
+            if (tal >= 900) return "CM" + ConvertNumToRoman(tal - 900);
 
-                if (tidigare != 0 && nutid > tidigare)
-                {
-                    totalt = totalt - (2 * tidigare) + nutid;
-                }
-                else
-                {
-                    totalt += nutid;
-                }
-                tidigareRomersk = nutidRomersk;
-            }
-            return totalt;
+            if (tal >= 500) return "D" + ConvertNumToRoman(tal - 500);
+
+            if (tal >= 400) return "CD" + ConvertNumToRoman(tal - 400);
+
+            if (tal >= 100) return "C" + ConvertNumToRoman(tal - 100);
+
+            if (tal >= 90) return "XC" + ConvertNumToRoman(tal - 90);
+
+            if (tal >= 50) return "L" + ConvertNumToRoman(tal - 50);
+
+            if (tal >= 40) return "XL" + ConvertNumToRoman(tal - 40);
+
+            if (tal >= 10) return "X" + ConvertNumToRoman(tal - 10);
+
+            if (tal >= 9) return "IX" + ConvertNumToRoman(tal - 9);
+
+            if (tal >= 5) return "V" + ConvertNumToRoman(tal - 5);
+
+            if (tal >= 4) return "IV" + ConvertNumToRoman(tal - 4);
+
+            if (tal >= 1) return "I" + ConvertNumToRoman(tal - 1);
+            
+            return String.Empty;
         }
-        
-        
-        //public static string ConvertNumToRoman(int tal)
+
+        //static Rom()
         //{
-        //    if (tal >=1000) return "M" + ConvertNumToRoman(tal - 1000);
-        //    if (tal >= 500) return "D" + ConvertNumToRoman(tal - 500);
-        //    if (tal >= 100) return "C" + ConvertNumToRoman(tal - 100);
-        //    if (tal >= 50) return "L" + ConvertNumToRoman(tal - 50);
-        //    if (tal >= 10) return "X" + ConvertNumToRoman(tal - 10);
-        //    if (tal >= 5) return "V" + ConvertNumToRoman(tal - 5);
-        //    if (tal >= 1) return "I" + ConvertNumToRoman(tal - 1);
+        //    LexikonRomerskaSiffror = new Dictionary<char, int>
+        //    {
+        //        {'M', 1000},
+        //        {'D', 500},
+        //        {'C', 100},
+        //        {'L', 50},
+        //        {'X', 10},
+        //        {'V', 5},
+        //        {'I', 1}
+        //    };
+
+        //    LexikonNummerTillRomerska = new Dictionary<int, string>
+        //    {
+        //        {1, "I"},
+        //        {5, "V"},
+        //        {10, "X"},
+        //        {50, "L"},
+        //        {100, "C"},
+        //        {500, "D"},
+        //        {1000, "M"}
+        //    };
         //}
+
+        //public static string Till(int tal)
+        //{
+        //    var romersk = new StringBuilder();
+
+        //    foreach (var sak in LexikonNummerTillRomerska)
+        //    {
+        //        while (tal >= sak.Key)
+        //        {
+        //            romersk.Append(sak.Value);
+        //            tal -= sak.Key;
+        //        }
+        //    }
+        //    return romersk.ToString();
+        //}
+
+        //public static int TagenAv(string romersk)
+        //{
+        //    int totalt = 0;
+        //    int nutid, tidigare = 0;
+        //    char nutidRomersk, tidigareRomersk = '\0';
+
+        //    for (int i = 0; i < romersk.Length; i++)
+        //    {
+        //        nutidRomersk = romersk[i];
+        //        tidigare = tidigareRomersk != '\0' ? LexikonRomerskaSiffror[tidigareRomersk] : '\0';
+        //        nutid = LexikonRomerskaSiffror[nutidRomersk];
+
+        //        if (tidigare != 0 && nutid > tidigare)
+        //        {
+        //            totalt = totalt - (2 * tidigare) + nutid;
+        //        }
+        //        else
+        //        {
+        //            totalt += nutid;
+        //        }
+        //        tidigareRomersk = nutidRomersk;
+        //    }
+        //    return totalt;
     }
+    
 }
